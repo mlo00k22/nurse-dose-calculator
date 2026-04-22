9d3fc9813b3632919e7088c058a7257761b08c83
mlo00k22 <abo0o00df16@gmail.com>
2026-04-22 03:13:14 +0300

    Update formulas.js

--- formulas.js
+++ formulas.js
@@ -1,4 +1,4 @@
-﻿const TREATMENTS = [
+const TREATMENTS = [
   {
     id: "norepinephrine",
     name: "NOR-EPINEPHRINE INFUSION",
@@ -9,6 +9,11 @@
     unitNote: "Order usually in mcg/kg/min",
     preparation: "38 ml D5W/NS + 12 ml Nor-epinephrine = 50 ml",
     mode: "weightedWithStock",
+    ranges: {
+      neonate: { icu: { min: 0.05, max: 1 }, emergency: { min: 0.1, max: 1 }, stable: { min: 0.05, max: 0.5 } },
+      pediatric: { icu: { min: 0.05, max: 1 }, emergency: { min: 0.1, max: 1 }, stable: { min: 0.05, max: 0.5 } },
+      adult: { icu: { min: 0.05, max: 1 }, emergency: { min: 0.1, max: 2 }, stable: { min: 0.05, max: 0.5 } }
+    }
   },
   {
     id: "dopamine",
@@ -20,6 +25,11 @@
     unitNote: "Order usually in mcg/kg/min",
     preparation: "45 ml D5W/NS + 5 ml Dopamine = 50 ml",
     mode: "weightedWithStock",
+    ranges: {
+      neonate: { icu: { min: 2, max: 20 }, emergency: { min: 5, max: 20 }, stable: { min: 2, max: 10 } },
+      pediatric: { icu: { min: 2, max: 20 }, emergency: { min: 5, max: 20 }, stable: { min: 2, max: 10 } },
+      adult: { icu: { min: 2, max: 20 }, emergency: { min: 5, max: 20 }, stable: { min: 2, max: 10 } }
+    }
   },
   {
     id: "rocuronium",
@@ -31,6 +41,11 @@
     unitNote: "Order usually in mcg/kg/min",
     preparation: "45 ml D5W/NS + 5 ml Rocuronium = 50 ml",
     mode: "weightedWithStock",
+    ranges: {
+      neonate: { icu: { min: 5, max: 12 }, emergency: { min: 8, max: 12 }, stable: { min: 5, max: 10 } },
+      pediatric: { icu: { min: 5, max: 12 }, emergency: { min: 8, max: 12 }, stable: { min: 5, max: 10 } },
+      adult: { icu: { min: 5, max: 12 }, emergency: { min: 8, max: 12 }, stable: { min: 5, max: 10 } }
+    }
   },
   {
     id: "ketamine",
@@ -42,6 +57,11 @@
     unitNote: "Order usually in mg/kg/min",
     preparation: "30 ml D5W/NS + 20 ml Ketamine = 50 ml",
     mode: "weightedWithStock",
+    ranges: {
+      neonate: { icu: { min: 0.05, max: 0.3 }, emergency: { min: 0.1, max: 0.5 }, stable: { min: 0.05, max: 0.2 } },
+      pediatric: { icu: { min: 0.05, max: 0.3 }, emergency: { min: 0.1, max: 0.5 }, stable: { min: 0.05, max: 0.2 } },
+      adult: { icu: { min: 0.05, max: 0.3 }, emergency: { min: 0.1, max: 0.5 }, stable: { min: 0.05, max: 0.2 } }
+    }
   },
   {
     id: "dobutamine",
@@ -53,6 +73,11 @@
     unitNote: "Order usually in mcg/kg/min",
     preparation: "45 ml D5W/NS + 5 ml Dobutamine = 50 ml",
     mode: "weightedWithStock",
+    ranges: {
+      neonate: { icu: { min: 2, max: 20 }, emergency: { min: 5, max: 20 }, stable: { min: 2, max: 10 } },
+      pediatric: { icu: { min: 2, max: 20 }, emergency: { min: 5, max: 20 }, stable: { min: 2, max: 10 } },
+      adult: { icu: { min: 2, max: 20 }, emergency: { min: 5, max: 20 }, stable: { min: 2, max: 10 } }
+    }
   },
   {
     id: "lasix",
@@ -64,6 +89,11 @@
     unitNote: "Order usually in mg/kg/hr",
     preparation: "20 ml D5W/NS + 10 ml Lasix = 30 ml",
     mode: "weightedWithStock",
+    ranges: {
+      neonate: { icu: { min: 0.1, max: 0.4 }, emergency: { min: 0.2, max: 0.5 }, stable: { min: 0.1, max: 0.3 } },
+      pediatric: { icu: { min: 0.1, max: 0.4 }, emergency: { min: 0.2, max: 0.5 }, stable: { min: 0.1, max: 0.3 } },
+      adult: { icu: { min: 0.1, max: 0.4 }, emergency: { min: 0.2, max: 0.5 }, stable: { min: 0.1, max: 0.3 } }
+    }
   },
   {
     id: "heparin",
@@ -74,16 +104,11 @@
     unitNote: "Use direct desired dose in units",
     preparation: "45 ml NS + 25,000 IU Heparin = 50 ml",
     mode: "quantityByConcentration",
-  },
-  {
-    id: "ammonul",
-    name: "AMMONUL PREPARATION",
-    stockAmount: 5000,
-    stockUnit: "mg",
-    quantityMl: 50,
-    unitNote: "Enter direct dose in mg",
-    preparation: "Reference ratio: 5,000 mg in 50 ml",
-    mode: "quantityByStock",
+    ranges: {
+      neonate: { icu: { min: 10, max: 20 }, emergency: { min: 15, max: 25 }, stable: { min: 10, max: 15 } },
+      pediatric: { icu: { min: 10, max: 20 }, emergency: { min: 15, max: 25 }, stable: { min: 10, max: 15 } },
+      adult: { icu: { min: 10, max: 20 }, emergency: { min: 15, max: 25 }, stable: { min: 10, max: 15 } }
+    }
   },
   {
     id: "fentanyl",
@@ -95,6 +120,11 @@
     unitNote: "Order usually in mcg/kg/hr",
     preparation: "20 ml D5W/NS + 10 ml Fentanyl = 30 ml",
     mode: "weightedWithStock",
+    ranges: {
+      neonate: { icu: { min: 1, max: 5 }, emergency: { min: 2, max: 7 }, stable: { min: 1, max: 3 } },
+      pediatric: { icu: { min: 1, max: 5 }, emergency: { min: 2, max: 7 }, stable: { min: 1, max: 3 } },
+      adult: { icu: { min: 1, max: 5 }, emergency: { min: 2, max: 7 }, stable: { min: 1, max: 3 } }
+    }
   },
   {
     id: "midazolam",
@@ -106,15 +136,11 @@
     unitNote: "Order usually in mcg/kg/min",
     preparation: "41 ml D5W/NS + 9 ml Midazolam = 50 ml",
     mode: "weightedWithStock",
-  },
-  {
-    id: "sandostatin",
-    name: "SANDOSTATIN INJECTION",
-    dilutionMl: 50,
-    timeFactor: 1,
-    unitNote: "Order usually in mcg/kg/hr",
-    preparation: "0.1 mg/ml, calculated without stock divisor",
-    mode: "weightedNoStock",
+    ranges: {
+      neonate: { icu: { min: 0.05, max: 0.2 }, emergency: { min: 0.1, max: 0.3 }, stable: { min: 0.05, max: 0.1 } },
+      pediatric: { icu: { min: 0.05, max: 0.2 }, emergency: { min: 0.1, max: 0.3 }, stable: { min: 0.05, max: 0.1 } },
+      adult: { icu: { min: 0.05, max: 0.2 }, emergency: { min: 0.1, max: 0.3 }, stable: { min: 0.05, max: 0.1 } }
+    }
   },
   {
     id: "epinephrine",
@@ -126,13 +152,10 @@
     unitNote: "Order usually in mcg/kg/min",
     preparation: "40 ml D5W/NS + 10 ml Epinephrine = 50 ml",
     mode: "weightedWithStock",
-  },
-  {
-    id: "diazepam",
-    name: "DIAZEPAM INJECTION",
-    conversionFactor: 0.2,
-    unitNote: "Do not dilute. 1 mg = 0.2 ml",
-    preparation: "Direct conversion only",
-    mode: "mgToMlConversion",
-  },
+    ranges: {
+      neonate: { icu: { min: 0.01, max: 0.3 }, emergency: { min: 0.05, max: 0.5 }, stable: { min: 0.01, max: 0.1 } },
+      pediatric: { icu: { min: 0.01, max: 0.3 }, emergency: { min: 0.05, max: 0.5 }, stable: { min: 0.01, max: 0.1 } },
+      adult: { icu: { min: 0.01, max: 0.5 }, emergency: { min: 0.05, max: 1 }, stable: { min: 0.01, max: 0.2 } }
+    }
+  }
 ];
